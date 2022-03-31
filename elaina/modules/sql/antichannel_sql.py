b4fab58a5c3b6c0c729c71cf996e80fd9353f06c
Zen-Prjkt <zenitsuprjkt@gmail.com>
2022-03-31 23:44:01 +0700

    .

--- elaina/modules/sql/antichannel_sql.py
+++ elaina/modules/sql/antichannel_sql.py
@@ -1,14 +1,37 @@
+# MIT License
+
+# Copyright (c) 2022 Zenitsu Prjkt™
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import threading
 
-from sqlalchemy import Boolean, Column
+from sqlalchemy import Boolean
 from sqlalchemy.sql.sqltypes import String
+from sqlalchemy import Column
 
 from elaina.modules.sql import BASE, SESSION
 
 
 class AntiChannelSettings(BASE):
     __tablename__ = "anti_channel_settings"
-
+    
     chat_id = Column(String(14), primary_key=True)
     setting = Column(Boolean, default=False, nullable=False)
 
@@ -22,7 +45,6 @@ class AntiChannelSettings(BASE):
 
 AntiChannelSettings.__table__.create(checkfirst=True)
 ANTICHANNEL_SETTING_LOCK = threading.RLock()
-
 
 def enable_antichannel(chat_id: int):
     with ANTICHANNEL_SETTING_LOCK:
